2d01ca3f06e3d44eb850e5b3c3590f606d1abadb
zsy <1541101353@qq.com>
2022-06-14 18:06:39 +0800

    111

--- src/main/java/com/zsy/app/ContainerWithMostWater.java
+++ src/main/java/com/zsy/app/ContainerWithMostWater.java
@@ -1,9 +1,5 @@
 package com.zsy.app;
 
-
-import java.lang.reflect.Array;
-import java.util.ArrayList;
-
 public class ContainerWithMostWater {
     public static int test1() {
         int[] height = new int[]{1,8,6,2,5,4,8,3,7};
@@ -15,21 +11,36 @@ public class ContainerWithMostWater {
         while(r>0) {
             area = Math.max(Math.min(height[i],height[j]) * (j - i),area);
             if(height[i] >= height[j]) {
-
                 j--;
             } else {
                 i++;
             }
             r--;
-
         }
         System.out.println(area);
         return area;
-
+    }
+    public static int test2() {
+        int i,j;
+        int[] height = new int[]{2,3,4,5,18,17,6};
+        int r = height.length;
+        i = 0;
+        j = height.length - 1;
+        int area = Math.min(height[i],height[j]) *  (j-i);
+        while((r-1) > 0) {
+            if (height[j] >= height[i]) {
+                i++;
+            } else {
+                j--;
+            }
+            area = Math.max(Math.min(height[i],height[j]) * Math.abs(j-i),area);
+            r--;
+        }
+        System.out.println(area);
+        return area;
     }
     public static void main(String[] args) {
-
-        test1();
+        test2();
     }
 
 }
